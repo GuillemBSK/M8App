afcb49ac0d0bb9d9927c1f86fc556aaeaf891ab2
DAM\GuillemMV <gmagrina1@institutjaumehuguet.cat>
2022-10-04 16:39:29 +0200

    Spinner Adapter

--- app/src/main/java/com/example/appm8/MainActivity.java
+++ app/src/main/java/com/example/appm8/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.appm8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner Valor_Spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Valor_Spinner = (Spinner) findViewById(R.id.ValorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.unitats, android.R.layout.simple_spinner_item);
+
+        Valor_Spinner.setAdapter(adapter);
     }
 }
